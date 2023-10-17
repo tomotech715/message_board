2adfea73aa53019b38e170b13897263f1b98a08e
qazxs <qazxs@192.168.10.102>
2023-10-17 10:34:42 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "messages")
 
 public class Message {
@@ -17,16 +25,16 @@ public class Message {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
+
     @Column(name = "title", length =255, nullable = false)
     private String title;
-    
+
     @Column(name = "content", length = 255, nullable = false)
     private String content;
-    
+
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
-    
+
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
@@ -69,9 +77,9 @@ public class Message {
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
-    
 
-    
-    
+
+
+
 
 }
